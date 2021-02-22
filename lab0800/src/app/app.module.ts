17f9d61c892e0c61860b7f14fddb95ecb598607d
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-02-22 11:51:44 +0700

    lab0800 done

--- lab0800/src/app/app.module.ts
+++ lab0800/src/app/app.module.ts
@@ -3,22 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+
+import { LoginComponent } from './login/login.component';
+import { MemberComponent } from './member/member.component';
+import { Member2Component } from './member2/member2.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import {Routes,RouterModule} from "@angular/router";
+
+
+const appRoutes: Routes = [
+  {path:'',component:MemberComponent},
+  {path:'login',component:LoginComponent},
+  {path:'Member1',component:MemberComponent},
+  {path:'Member2',component:Member2Component}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MemberComponent,
+    HeaderComponent,
+    FooterComponent,
+    Member2Component
+   
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CardModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    ToolbarModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
