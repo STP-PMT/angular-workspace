17de956dc36e1b4c87f0c48ea27ced6befc95014
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-03-01 00:32:43 +0700

    สามารถลิงก์ได้

--- member/src/app/app.module.ts
+++ member/src/app/app.module.ts
@@ -6,12 +6,25 @@ import {AccordionModule} from 'primeng/accordion';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ToolbarModule } from 'primeng/toolbar';
 
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { Member1Component } from './member1/member1.component';
 import { Member2Component } from './member2/member2.component';
 import { Member3Component } from './member3/member3.component';
 import { Member4Component } from './member4/member4.component';
+import { HomeComponent } from './home/home.component';
+
+import {Routes,RouterModule} from "@angular/router";
+
+
+const appRoutes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'Member1',component:Member1Component},
+  {path:'Member2',component:Member2Component},
+  {path:'Member3',component:Member3Component},
+  {path:'Member4',component:Member4Component},
+];
 
 @NgModule({
   declarations: [
@@ -21,13 +34,17 @@ import { Member4Component } from './member4/member4.component';
     Member1Component,
     Member2Component,
     Member3Component,
-    Member4Component
+    Member4Component,
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
     AccordionModule,
     BrowserAnimationsModule,
-    ToolbarModule
+    ToolbarModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
